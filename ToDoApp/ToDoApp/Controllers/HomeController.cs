a892ac117bdeb22de2d9153cdba5d178c26c5277
Toth Zoltan2018 <toth.tozso.zoltan@gmail.com>
2018-11-30 12:19:18 +0100

    #2:Sajat Controller letrehozasa, action athelyezese, es atnevezese Ujra, most mar nem toroltem a 3 fajlt

--- ToDoApp/ToDoApp/Controllers/HomeController.cs
+++ ToDoApp/ToDoApp/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ToDoApp.Models;
+
 
 namespace ToDoApp.Controllers
 {
@@ -27,35 +27,5 @@ namespace ToDoApp.Controllers
 
             return View();
         }
-
-        public ActionResult TodoList()
-        {
-            //------- Ez volt az megoldas #1 Mukodott, de eleg proszto --------
-            //var lista = new List<string>();
-            //lista.Add("Só");
-            //lista.Add("Cukor");
-            //lista.Add("Spagetti");
-            //lista.Add("Marhahús");
-            //lista.Add("Paradicsom");
-
-            //A ViewBag-be tett adatokat a nezeten ki tudjuk olvasni
-            //figyelem:az erosen tipusos vedelmet itt elveszitjuk
-            //ViewBag.Lista = lista;
-            //return View();
-
-            //------- Ez a megoldas #2 ---------------------
-            //A TodoList.cshtml elejere: @model List<string>
-            //return View(lista);
-            //---------------------------------------------------
-
-            var lista = new List<TodoItem>();
-            lista.Add(new TodoItem() { Name = "Só", Done = true});
-            lista.Add(new TodoItem() { Name = "Cukor", Done = true });
-            lista.Add(new TodoItem() { Name = "Spagetti", Done = true });
-            lista.Add(new TodoItem() { Name = "Marhahús", Done = false });
-            lista.Add(new TodoItem() { Name = "Paradicsom", Done = false });
-
-            return View(lista);
-        }
-    }
+     }
 }
